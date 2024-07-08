faee026e86a8f4cffa64418422ff67ee2c969bc5
gachempa <jpsg514@gmail.com>
2024-07-08 09:21:03 -0700

    testing only

--- LeetPracticeEtc/Set_2/testing.py
+++ LeetPracticeEtc/Set_2/testing.py
@@ -1,13 +1,71 @@
 # for hard linked list prob - reverse k list
 
-        cycle=0
-        a=b=c=z=head
-        b=a.next
-        c=b.next
+if k==1: return head
 
-        def kloop(a,b,c,z,head):
-            for i in range(k-1):
-                z=z.next
+cycle=0
+a0=a=b=c=z=head
+b=a.next
+c=b.next
 
-            if z==None: return head
-            if cycle==0: head=z
+def kloop(a0,a,b,c,z,head,cycle):
+
+    for i in range(k-1):
+        if not z.next: 
+            return head
+        else: 
+            z=z.next
+        
+    if cycle==0: 
+        head=z            
+    else:
+        cycle+=1
+    # return head            
+    for i in range(k):
+        if i==k-1:
+            if a.next:
+                a0.next=z
+                a=a.next
+                z=a
+                b=a.next
+                c=b.next
+                kloop(a0,a,b,c,z,head,cycle)
+            else:
+                return head
+        else:
+            b.next=a
+            a=b
+            b=c
+            c=c.next
+    
+return kloop(a0,a,b,c,z,head,cycle)
+
+
+# another try
+
+        if k==1: return head
+
+        a=b=k2=head
+        x=1
+        y=0
+
+        for i in range(k-1):
+            k2=k2.next
+            head=head.next
+            x+=1
+
+        b=b.next
+
+        while x>=0:
+            x-=1
+            if k2.next: 
+                k2=k2.next
+                y+=1
+
+            a.next=b.next
+            b.next=a
+            a=a.next
+            b=b.next
+
+            if x==0 and y==k:
+                x=k
+                y=0
--- LeetPracticeEtc/trials.py
+++ LeetPracticeEtc/trials.py
@@ -1,60 +1,7 @@
-#  works but need to spped ip ... another version for that
+#  test
+n=5
+k=2
 
-nums = [-2,0,0,2,2]
+q,r=divmod(n,k)
 
-ans_list=[]
-add_flag=0
-nums_freq={}
-for n in nums:
-    if n in nums_freq:
-        nums_freq[n]+=1
-    else:
-        nums_freq[n]=1
-# print(nums_freq)
-
-if 0 in nums_freq and nums_freq[0]>=3:
-    ans_list.append([0,0,0])
-
-for i in range(len(nums)-2):
-    x=nums[i]
-    sum=0-x
-    # print("sum",sum)
-    for j in range(i+1,len(nums)-1):
-        jsum=sum-nums[j]
-        if jsum in nums_freq:
-                     
-            if (jsum==nums[j] and jsum==nums[i]):
-                if nums_freq[jsum]>2: add_flag=1
-
-            elif (jsum==nums[j] or jsum==nums[i]):
-                  if nums_freq[jsum]>1: add_flag=1
-
-            elif (jsum!=nums[j] and jsum!=nums[i]):
-                add_flag=1
-            
-            if add_flag==1:
-                a=sorted([nums[i],nums[j],jsum])
-                if a not in ans_list: ans_list.append(a)
-                add_flag=0
-
-            print("i, j, jsum:",i,j,nums[i],nums[j], jsum)
-            print("a",a)
-            
-            # or \
-            #     ((jsum==nums[j] or jsum==nums[i]) and nums_freq[jsum]>1) or \
-            #         (jsum!=nums[j] and jsum!=nums[i]):
-            #     a=sorted([nums[i],nums[j],jsum])
-            #     if a not in ans_list:
-            #         ans_list.append(a)
-            #         print("i, j, jsum:",i,j,nums[i],nums[j], jsum)
-            #         print("a",a)
-
-            # elif jsum!=nums[j] and jsum!=nums[i]:
-            #     a=sorted([nums[i],nums[j],jsum])
-            #     if a not in ans_list:
-            #         ans_list.append(a)
-                # print("a",a)
-        # print("jsum",jsum)
-    # print(sum)
-
-print(ans_list)
+print(q,r)
