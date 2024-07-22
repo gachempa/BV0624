bed5ef4f560787d9f81ac1b4527b8e20125d6f29
gachempa <jpsg514@gmail.com>
2024-07-22 14:20:09 -0700

    Sudoku recursion done ....

--- LeetPracticeEtc/Hard/Sudoku.py
+++ LeetPracticeEtc/Hard/Sudoku.py
@@ -1,49 +1,32 @@
 board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
-
 i, j = 0, 0
 nums=["1","2","3","4","5","6","7","8","9"]
 
-# print(board[i][j])
 
 def gfill(i=0,j=0,board=board):
+    if j==9:
+        i+=1
+        j=0
+    if i==9:
+        return True
 
-    if i<9:
-        if board[i][j]==".":
-            print("i","j",i,j)
-            exclude = gseg(i,j,board) | gcol(j,board) | grow(i,board)
-            print("col:",gcol(j,board))
-            print("exclude:",exclude)
-            if set(nums).issubset(exclude):
-                print("False")
-                return False
-            else:
-                for num in nums:
-                    if num not in exclude:
-                        board[i][j]=num
-                        print("i j num:",i,j,num)
-                        j+=1
-                        if j==9:
-                            i+=1
-                            if i == 9: return True
-                            j=0
-                        gfill(i,j,board)                        
-                print(i,j)
+    elif board[i][j]==".":
+        exclude = gseg(i,j,board) | gcol(j,board) | grow(i,board)
+        if set(nums).issubset(exclude):
+            return False 
         else:
-            print("else","i","j",i,j)
-            j+=1
-            if j==9:
-                i+=1
-                if i == 9: return True
-                j=0
-            gfill(i,j,board)         
+            for num in nums:           
+                if num not in exclude:
+                    board[i][j]=num
+                    if gfill(i,j+1,board):return True        
+                    else: board[i][j]="."
 
-    # else:
-    #     return False
+    elif gfill(i,j+1,board):
+            return True
 
 def grow(i,board):
     grow1=set()
     for c in range(9):
-        # grow1.append(board[i][c])
         grow1.add(board[i][c])
     return grow1
 
@@ -62,12 +45,15 @@ def gseg(i,j,board):
             gseg1.add(board[x][y])
     return gseg1
 
-
 gfill()
-# lrow=grow(0,board)
-# print(lrow)
-
 print(board)
+
+# lrow=grow(7,board)
+# print(lrow)
+# lcol=gcol(7,board)
+# print(lcol)
+# lseg=gseg(7,3,board)
+# print(lseg)
 
 # exclude=gseg(0,0,board) | gcol(0,board) | grow(0,board)
 # print(exclude)
--- LeetPracticeEtc/RecursionTests.py
+++ LeetPracticeEtc/RecursionTests.py
@@ -1,8 +1,32 @@
-def gridPaths(n,m):
-    if n==1 or m==1:
-        return 1
-    else:
-        gridPaths(n,m-1)+gridPaths(n-1,m)
+board = [["5","3",".",".","7",".",".",".","."],["6",".","1","2",".","5",".","8","9"]]
 
-p= gridPaths(3,3)
-print(p)
+i, j = 0, 0
+nums=["1","2","3","4","5","6","7","8","9"]
+
+def mtree(i,j,board):
+    if j==9:
+        i+=1
+        j=0
+
+    print("i,j,board",i,j,board)
+
+    if i==2:
+        return board
+    
+    if i<1 and j>0:
+        if board[i][j-1]==board[i+1][j-1]:
+            return False
+    
+    if board[i][j]==".":
+        for num in nums:
+            if num not in board[i]:
+                board[i][j]=num
+                # print("i, j, num, board: ", i,j,num, board)
+                if mtree(i,j+1,board):
+                    return True
+    else: 
+        mtree(i,j+1,board)
+
+
+mtree(0,0,board)
+print(board)
