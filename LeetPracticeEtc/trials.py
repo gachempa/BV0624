9af4f316b6c8f23937bd5e12cfb0f2068e33ea04
gachempa <jpsg514@gmail.com>
2024-07-28 14:25:56 -0700

    wird search77

--- LeetPracticeEtc/trials.py
+++ LeetPracticeEtc/trials.py
@@ -1,8 +1,14 @@
 #  test
 
-lst = [[[x for x in range[5] for x in range(5)] for x in range(5)]]
 
-print(lst)
+
+# n=4
+# l = [[1, 3, 0, 2], [2, 0, 3, 1]]
+# s = [["."*(x)+"Q"+"."*(n-x-1) for x in y] for y in l]
+# print(s)
+
+# lst = [[[x for x in range(5)] for x in range(5)] for x in range(5)]
+# print(lst)
 
 # nums=["1","2","3","4","5","6","7","8","9"]
 # exclude= {'3', '5', '6', '8', '4', '1', '7', '9', '2', '.'}
