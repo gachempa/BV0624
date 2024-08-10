5580149d05aa4d2b2d1d29c09c449cc07e46fe4d
gachempa <jpsg514@gmail.com>
2024-08-09 21:25:22 -0700

    bfs2

---
+++ LeetPracticeEtc/BFS-2.py
@@ -0,0 +1,28 @@
+# BFS general approach: https://favtutor.com/blogs/breadth-first-search-python
+
+graph = {
+    '5' : ['3', '7'],
+    '3' : ['2', '4'],
+    '7' : ['8'],
+    '2' : [],
+    '4' : ['8'],
+    '8' : [],
+}
+
+visited = []
+queue = []
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        m = queue.pop(0)
+        print(m,end = " ")
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+print("BFS nodes:")
+bfs(visited, graph, node='5')
