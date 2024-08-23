5917a31fe456de62e90dcc0f419db4f568e589d8
gachempa <jpsg514@gmail.com>
2024-08-23 09:37:04 -0700

    using return and map

--- LeetPracticeEtc/JustPractices/SomeTips1.py
+++ LeetPracticeEtc/JustPractices/SomeTips1.py
@@ -1,12 +1,22 @@
 # from variouys youtubes
 
-people = [
-    {"name": "A", "age":1},
-    {"name": "D", "age":2},
-    {"name": "C", "age":3}
-]
+def square_it(n):
+    return n*n
 
-people.sort(key=lambda person:person["name"])
+x=[3,5,3,2,8,1]
 
-for p in people:
-    print(f"{p['name']}:{p['age']}")
+y=list(map(square_it,x))
+print(y)
+
+
+
+# people = [
+#     {"name": "A", "age":1},
+#     {"name": "D", "age":2},
+#     {"name": "C", "age":3}
+# ]
+
+# people.sort(key=lambda person:person["name"])
+
+# for p in people:
+#     print(f"{p['name']}:{p['age']}")
