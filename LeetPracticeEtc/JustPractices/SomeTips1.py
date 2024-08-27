593a0440af394d4f0701d5be970557985f9d5f85
gachempa <jpsg514@gmail.com>
2024-08-27 11:07:51 -0700

    map function

--- LeetPracticeEtc/JustPractices/SomeTips1.py
+++ LeetPracticeEtc/JustPractices/SomeTips1.py
@@ -8,8 +8,12 @@ x=[3,5,3,2,8,1]
 y=list(map(square_it,x))
 print(y)
 
+def mu_len(n):
+    return len(n)
 
+x2 = map(mu_len, ("rock", "paper", "scissors"))
 
+print(list(x2))
 # people = [
 #     {"name": "A", "age":1},
 #     {"name": "D", "age":2},
