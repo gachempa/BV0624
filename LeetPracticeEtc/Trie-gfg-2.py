92f008bc5285367a16d266c214fdb1104dfd368d
gachempa <jpsg514@gmail.com>
2024-09-04 08:39:01 -0700

    trie work

--- LeetPracticeEtc/Trie-gfg-2.py
+++ LeetPracticeEtc/Trie-gfg-2.py
@@ -15,3 +15,24 @@ def insert_key(root, key):
         curr=curr.child[index]
     curr.wordEnd = True
 
+def searchKey(root, key):
+    curr = root
+    for c in key:
+        index = ord(c) - ord('a')
+        if curr.child[index] is None:
+            return False
+        curr = curr.child[index]
+    return curr.wordEnd
+
+root = Trienode()
+arr = ['and','ant','do','geek','dad','ball']
+for s in arr:
+    insert_key(root, s)
+
+searchKey = ["do","gee","bat"]
+for s in searchKey:
+    print(f"Key : {s}")
+    if searchKey(root, s):
+        print("Present")
+    else:
+        print("not Present")
