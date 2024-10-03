718775c30e549b8d4d06874088eefccc8e1d00df
gachempa <jpsg514@gmail.com>
2024-10-03 11:13:41 -0700

    done LC 212

--- LeetPracticeEtc/Hard/LC-212-WordSearch2-Trie.py
+++ LeetPracticeEtc/Hard/LC-212-WordSearch2-Trie.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/word-search-ii/description/
 
+board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+words = ["oath","pea","eat","rain"]
+
 class Trienode:
     def __init__(self) -> None:
         self.children = {}
@@ -14,13 +17,37 @@ class Trie:
         for char in word:
             if char not in currnode.children:
                 currnode.children[char] = Trienode()
+                print(char)
             currnode = currnode.children[char]
         currnode.is_word_end = True
 
-board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
-words = ["oath","pea","eat","rain"]
+def findWords(board, words):
+    trie_ = Trie()
+    for word in words:
+        trie_.insert(word)
+        print("hhh")
+    rows, cols = len(board), len(board[0])
+    visited = [[False]*cols for _ in range(rows)]
+    ans = []
 
-trie_ = Trie()
-for word in words:
-    trie_.insert(word)
+    def dfs(r, c, currnode, word):
+        if r<0 or r>=rows or c<0 or c>=cols or visited[r][c] or board[r][c] not in currnode.children:
+            return
+        visited[r][c]=True
+        currnode = currnode.children[board[r][c]]
+        word += board[r][c]
+        if currnode.is_word_end:
+            ans.append(word)
+            currnode.is_word_end = False # so the word will not be appended again to ans
+        dfs(r+1,c,currnode,word)
+        dfs(r-1,c,currnode,word)
+        dfs(r,c+1,currnode,word)
+        dfs(r,c-1,currnode,word)
+        visited[r][c]=False
 
+    for i in range(rows):
+        for j in range(cols):
+            dfs(i,j,trie_.root,"")
+    print(ans)
+
+findWords(board,words)
