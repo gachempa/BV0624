4d380f57b580f87f483ac5bb921b80f95a53cede
gachempa <jpsg514@gmail.com>
2024-10-10 10:19:58 -0700

    done

--- LeetPracticeEtc/Hard/LC218-skyline.py
+++ LeetPracticeEtc/Hard/LC218-skyline.py
@@ -6,17 +6,18 @@ prev_ht = 0
 
 starts_min=min([innerlist[0] for innerlist in buildings])
 ends_max=max([innerlist[1] for innerlist in buildings])
+buildings.append((starts_min,ends_max+1,0)) #needed to take care of 0 height
 print(starts_min,ends_max)
 
-for i in range(starts_min,ends_max):
+for i in range(starts_min,ends_max+1):
     y_ht = max([innerlist[2] for innerlist in buildings 
-                if innerlist[0] <= i for innerlist in buildings 
-                if innerlist[1] > i for innerlist in buildings ]) 
+                if innerlist[0] <= i  
+                if innerlist[1] > i ]) 
     if y_ht == prev_ht:
         pass
     else:
         ans.append((i, y_ht))
         prev_ht = y_ht
-    print(i)
+    print(i, y_ht)
 
 print(ans)
