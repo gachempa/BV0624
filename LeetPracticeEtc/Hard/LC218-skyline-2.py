adb649134f3ef0869e42af33b6f4a799bb7abde0
gachempa <jpsg514@gmail.com>
2024-10-11 10:24:07 -0700

    lc218 done submitted

--- LeetPracticeEtc/Hard/LC218-skyline-2.py
+++ LeetPracticeEtc/Hard/LC218-skyline-2.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/the-skyline-problem/description/
+# optimized 
 
 buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
 ans=[]
@@ -7,16 +8,22 @@ prev_ht = 0
 starts_min=min([innerlist[0] for innerlist in buildings])
 ends_max=max([innerlist[1] for innerlist in buildings])
 buildings.append((starts_min,ends_max+1,0)) #needed to take care of 0 height
-# print(starts_min,ends_max)
+# points to iterate at
+itr_pts = set()
+for i in range(len(buildings)):
+    itr_pts.add(buildings[i][0])
+    itr_pts.add(buildings[i][1])
+sorted_itr_pts = sorted(list(itr_pts))
+# print(sorted_itr_pts)
 
-for i in range(starts_min,ends_max+1):
+for i in range(len(sorted_itr_pts)-1):
     y_ht = max([innerlist[2] for innerlist in buildings 
-                if innerlist[0] <= i  
-                if innerlist[1] > i ]) 
+                if innerlist[0] <= sorted_itr_pts[i]  
+                if innerlist[1] > sorted_itr_pts[i] ]) 
     if y_ht == prev_ht:
         pass
     else:
-        ans.append((i, y_ht))
+        ans.append((sorted_itr_pts[i], y_ht))
         prev_ht = y_ht
     # print(i, y_ht)
 
