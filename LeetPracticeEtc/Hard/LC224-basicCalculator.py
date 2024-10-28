90f4ec2dfb3c7ff4be36342b648ddd5b98ea25f6
gachempa <jpsg514@gmail.com>
2024-10-27 22:02:32 -0700

    lc224 done

--- LeetPracticeEtc/Hard/LC224-basicCalculator.py
+++ LeetPracticeEtc/Hard/LC224-basicCalculator.py
@@ -1,8 +1,13 @@
 # https://leetcode.com/problems/basic-calculator/description/
+import re
 
 s = "(1+(4+5+2) -  3)+(6+8)"
 
 s = f'({s.replace(" ","")})'
 
+x = lambda y:str(sum(map(int,re.findall(r'[+-]?\d+',y[0]))))
 
-print(s)
+while '(' in s:
+    s = re.sub(r'\([^()]+\)',x,s).replace("--","+")
+
+print(int(s))
